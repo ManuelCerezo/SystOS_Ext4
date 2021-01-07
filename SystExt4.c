e6d9774557b797fe1eaf680a4ca949efd4681c16
Miguel <73239423+MiguelHGDC@users.noreply.github.com>
2021-01-07 14:31:29 +0100

    Add files via upload

--- SystExt4.c
+++ SystExt4.c
@@ -13,6 +13,7 @@
 #define LONGITUD_ARGUMENTOS 20 //No es necesario +20 caracteres para definir un argumento.
 
 //Funciones.
+void imprimirDir(EXT_ENTRADA_DIR *Pdirectorio,EXT_BLQ_INODOS *Pext_blq_inodos);
 void ImprimirByteMaps(EXT_BYTE_MAPS *Pbyte_map);
 void ActualizarSuperBloque(EXT_SIMPLE_SUPERBLOCK *Psuper_bloque,EXT_DATOS *particion);
 void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *Psuper_bloque);
@@ -25,7 +26,7 @@ int main(){
     EXT_ENTRADA_DIR directorio[MAX_FICHEROS];     //Definicion de la estructura directorio
     EXT_DATOS memdatos[MAX_BLOQUES_DATOS];        //Definicion de la estructura datos libres
     EXT_DATOS particion[MAX_BLOQUES_PARTICION];   //Array de estructuras que contienen bloques de particion
-
+    
     int flag = 0;
     char comando[LONGITUD_COMANDO]; //buffer de comando.
     char Argumento1[LONGITUD_ARGUMENTOS];
@@ -38,10 +39,13 @@ int main(){
     if ((pfile = fopen("particion.bin", "r+b")) == NULL){ // "rb" -> Lectura solo del fichero binario
         printf ( " Error en la apertura\n ");
     }
-
-
     fread(&particion, SIZE_BLOQUE, MAX_BLOQUES_PARTICION, pfile); //Tenemos toda la particion dividida en la estructura de bloques.
     //Hacemos los cambios en la particion pero vamos guardandolo en el Pfile.
+     memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&particion[0], SIZE_BLOQUE);
+     memcpy(&directorio,(EXT_ENTRADA_DIR *)&particion[3], SIZE_BLOQUE);
+     memcpy(&ext_bytemaps,(EXT_BLQ_INODOS *)&particion[1], SIZE_BLOQUE);
+     memcpy(&ext_blq_inodos,(EXT_BLQ_INODOS *)&particion[2], SIZE_BLOQUE);
+     memcpy(&memdatos,(EXT_DATOS *)&particion[4],MAX_BLOQUES_DATOS*SIZE_BLOQUE);
     
     // Seleccion de comandos: info, bytemaps,dir,rename,imprimir,remove,copy,salir
     //Ejecucion del programa-----------------
@@ -55,30 +59,27 @@ int main(){
         memset(Argumento2, 0, LONGITUD_ARGUMENTOS);
         memset(comando,0,LONGITUD_COMANDO);
         do{
-        printf("\n>> ");scanf("%[^\n]s",comando);fpurge(stdin);  //[\n] indica la limitacion de lectura
+        printf("\n>> ");
+		scanf("%[^\n]s",comando);//[\n] indica la limitacion de lectura
+		fflush(stdin);  
         }while(comando[0]=='\0');
         desfracomando(comando,orden,Argumento1,Argumento2); //Funcion para desfragmentar el comando.
 
         if(strcmp(orden, "info")==0){ //Llamamos a la funcion correspondiente de cada uno.
             //printf("%s",orden); //Funcion de info:
-
-            memcpy(&ext_superblock,(EXT_SIMPLE_SUPERBLOCK *)&particion[0], SIZE_BLOQUE);
             LeeSuperBloque(&ext_superblock);
             //memcpy(&particion[0],(EXT_DATOS *)&ext_superblock, SIZE_BLOQUE);
 
             flag = 0;
         }else if(strcmp(orden, "bytemaps")==0){
             //printf("%s\n",orden); //Funcion de bytemaps
-
-            memcpy(&ext_bytemaps,(EXT_BLQ_INODOS *)&particion[1], SIZE_BLOQUE);
             //inicializarInodos(&ext_bytemaps);
             ImprimirByteMaps(&ext_bytemaps);
 
             flag = 0;
         
         }else if(strcmp(orden, "dir")==0){
-            memcpy(&directorio,(EXT_ENTRADA_DIR *)&datosfich[3], SIZE_BLOQUE);
-
+            imprimirDir(directorio,&ext_blq_inodos);
             //printf("%s",orden);
             flag = 0;
         
@@ -112,17 +113,38 @@ int main(){
     return 0;
 }
 
-void imprimirDir(EXT_ENTRADA_DIR *Pdirectorio){
-
-
-}
-
-void inicializarInodos(EXT_BYTE_MAPS *Pbyte_map){
+void imprimirDir(EXT_ENTRADA_DIR *Pdirectorio,EXT_BLQ_INODOS *Pext_blq_inodos){
     int i = 0;
-    for(i=0;i<MAX_INODOS;i++){
-       Pbyte_map->bmap_inodos[i]=0;
-    }
+    int n = 0;
+    int j = 0;
+
+    /* PARA DEBUGEAR
+    for(i=0;i<MAX_FICHEROS;i++){
+        if(Pdirectorio[i].dir_inodo != NULL_INODO){
+            printf("%s\t",Pdirectorio[i].dir_nfich);
+            printf("Inodo: %d Contador: %d\n",Pdirectorio[i].dir_inodo,i);
+
+        }
+        else{
+            printf("NULL, contador: %d\n",i);
+        }
+    }*/
     
+   for(i=1;i<MAX_FICHEROS;i++){   
+        if(Pdirectorio[i].dir_inodo != NULL_INODO){
+            printf("%s\t",Pdirectorio[i].dir_nfich);                                                    //nombre
+            printf("Inodo: %d\t",Pdirectorio[i].dir_inodo);                                             // Inodo
+            printf("Tamanio: %d\t",Pext_blq_inodos->blq_inodos[Pdirectorio[i].dir_inodo].size_fichero); //Tamanio
+            printf("Bloques: ");
+            for(n=0;n<MAX_NUMS_BLOQUE_INODO;n++){                                                       //MAX_NUMS_BLOQUE_INODO = 7    
+                if(Pext_blq_inodos->blq_inodos[Pdirectorio[i].dir_inodo].i_nbloque[n] != NULL_BLOQUE){
+                printf(" %d ",Pext_blq_inodos->blq_inodos[Pdirectorio[i].dir_inodo].i_nbloque[n]);      //bloques
+                }
+            }
+            printf("\n");
+       } 
+    }
+
 }
 
 void ImprimirByteMaps(EXT_BYTE_MAPS *Pbyte_map){
@@ -135,14 +157,14 @@ void ImprimirByteMaps(EXT_BYTE_MAPS *Pbyte_map){
     }
     printf("\nByte_Maps_Bloques [0-25]: ");
     for(i=0;i<25;i++){
-        printf("%d ",Pbyte_map->bmap_inodos[i]);
+        printf("%d ",Pbyte_map->bmap_bloques[i]);
     }
 }
 
 void LeeSuperBloque(EXT_SIMPLE_SUPERBLOCK *Psuper_bloque){ //posible funcion de actualizado de superbloque.
 
     printf("    -INFO-\n\n");
-    printf("Tamaño Bloque: %d\n",Psuper_bloque->s_block_size);
+    printf("Tamanio Bloque: %d\n",Psuper_bloque->s_block_size);
     printf("Inodos Particion: %d\n",Psuper_bloque->s_inodes_count);
     printf("Inodos Libres: %d\n",Psuper_bloque->s_free_inodes_count);
     printf("Bloques Particion: %d\n",Psuper_bloque->s_blocks_count);
